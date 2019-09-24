043bd78bb487b20a0b22a0bb579c8f124bd0e1ec
Bruno Martins Crocomo <bruno.crocomo@gmail.com>
2019-09-24 09:41:46 -0300

    Fix bug on wrapper overflow bug

--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
+    overflow: auto;
     height: 100%;
     background: linear-gradient(180deg, #22202c 0%, #402845 100%);
 `;
